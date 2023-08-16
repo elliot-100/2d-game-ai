a1a3d5b3847d888009f0089009b4893308a875e6
elliot-100 <3186037+elliot-100@users.noreply.github.com>
2023-08-16 20:21:29 +0100

    changed: on Bot arrival at destination, stop and delete destination

--- src/maths.py
+++ src/maths.py
@@ -16,3 +16,23 @@ def relative_bearing_degrees(heading: Vector2, displacement: Vector2) -> float:
     elif angle <= -CIRCLE_DEGREES / 2:
         angle += CIRCLE_DEGREES
     return angle
+
+
+def point_in_or_on_circle(
+    point: Vector2,
+    circle_centre: Vector2,
+    circle_radius: float,
+) -> bool:
+    """Determine if a point is (inside or on) a circle.
+
+    Parameters
+    ----------
+    point
+    circle_centre
+    circle_radius
+
+    Returns
+    -------
+    True if the point is (inside or on) the circle
+    """
+    return (point - circle_centre).length() <= circle_radius
