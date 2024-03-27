d30e0c24ba895d1575de61e13ee2c410a1ff16b1
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-03-27 19:16:27 +0000

    Implement Bearing class as vector

---
+++ tests/test_bearing.py
@@ -0,0 +1,65 @@
+"""Tests for Bearing class."""
+
+from pygame import Vector2
+
+from two_d_game_ai.bearing import Bearing
+
+VECTOR_NORTH = Vector2(0, 1)
+VECTOR_EAST = Vector2(1, 0)
+VECTOR_SOUTH = Vector2(0, -1)
+VECTOR_WEST = Vector2(-1, 0)
+
+
+def test_bearing_create_cardinal() -> None:
+    """Test that cardinal vector bearings are created correctly from degrees."""
+    degs = [0, 90, 180, 270]
+    assert [Bearing(d).vector for d in degs] == [
+        VECTOR_NORTH,
+        VECTOR_EAST,
+        VECTOR_SOUTH,
+        VECTOR_WEST,
+    ]
+
+
+def test_bearing_create_cardinal_large_values() -> None:
+    """Test that cardinal vector bearings are created correctly from degrees >= 360."""
+    degs = [360, 450, 540, 630, 720]
+    assert [Bearing(d).vector for d in degs] == [
+        VECTOR_NORTH,
+        VECTOR_EAST,
+        VECTOR_SOUTH,
+        VECTOR_WEST,
+        VECTOR_NORTH,
+    ]
+
+
+def test_bearing_create_cardinal_negative_values() -> None:
+    """Test that cardinal vector bearings are created correctly from degrees < 0."""
+    degs = [-360, -270, -180, -90]
+    assert [Bearing(d).vector for d in degs] == [
+        VECTOR_NORTH,
+        VECTOR_EAST,
+        VECTOR_SOUTH,
+        VECTOR_WEST,
+    ]
+
+
+def test_bearing_cardinal_degrees_preserved() -> None:
+    """# TODO."""
+    degs = [0, 90, 180, 270]
+    assert [Bearing(d).degrees for d in degs] == [0, 90, 180, 270]
+
+
+def test_relative() -> None:
+    """Test that relative bearings are calculated correctly."""
+    vecs = [
+        VECTOR_NORTH,
+        VECTOR_EAST,
+        VECTOR_SOUTH,
+        VECTOR_WEST,
+    ]
+    # relative bearings from NORTH:
+    assert [Bearing(0).relative(v).degrees for v in vecs] == [0, 90, 180, 270]
+    # relative bearings from SOUTH:
+    # TODO: close but failing
+    assert [Bearing(180).relative(v).degrees for v in vecs] == [180, 270, 0, 90]
