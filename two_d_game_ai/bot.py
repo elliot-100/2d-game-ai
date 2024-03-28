80723935052b0f41276f86d4289209ed0e1b723a
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-03-28 16:35:21 +0000

    Bot refactors

--- two_d_game_ai/bot.py
+++ two_d_game_ai/bot.py
@@ -83,29 +83,13 @@ class Bot(Subject):
         return False
 
     @property
-    def max_rotation_delta(self) -> float:
+    def max_rotation_step(self) -> float:
         """Return maximum rotation in one simulation step."""
         return self.MAX_ROTATION_RATE * SIMULATION_STEP_INTERVAL_S
 
-    def move(self) -> None:
-        """Change Bot position over 1 simulation step."""
-        self.pos += self.velocity * SIMULATION_STEP_INTERVAL_S
-
     def update(self, other_bots: list[Bot]) -> None:
         """Update Bot, including move over 1 simulation step."""
-        currently_visible_bots = {bot for bot in other_bots if self.can_see(bot)}
-        newly_spotted_bots = currently_visible_bots - self.visible_bots
-        newly_lost_bots = self.visible_bots - currently_visible_bots
-
-        if newly_spotted_bots:
-            for newly_spotted_bot in newly_spotted_bots:
-                self.notify_observers(f"I've spotted `{newly_spotted_bot.name}`")
-                self.visible_bots.add(newly_spotted_bot)
-        if newly_lost_bots:
-            for newly_lost_bot in newly_lost_bots:
-                self.notify_observers(f"I've lost sight of `{newly_lost_bot.name}`")
-                self.visible_bots.remove(newly_lost_bot)
-                self.known_bots.add(newly_lost_bot)
+        self._handle_sensing(other_bots)
 
         if self.is_at_destination:
             self.notify_observers("I've reached destination")
@@ -119,24 +103,52 @@ class Bot(Subject):
             ).degrees_normalised
 
             #  if Bot can complete rotation to face destination this step...
-            if self.max_rotation_delta >= abs(destination_relative_bearing):
+            if abs(destination_relative_bearing) <= self.max_rotation_step:
                 # face destination precisely
-                # NB legacy use of Pygame CCW rotation here, thus negative angle:
-                self.heading.vector.rotate_ip(-destination_relative_bearing)
-                # move towards destination
+                self.rotate(-destination_relative_bearing)
+                # initiate move towards destination
                 self.velocity = self.heading.vector * Bot.MAX_SPEED
 
             else:
                 # turn towards destination
-                self.heading.vector.rotate_ip(
+                self.rotate(
                     math.copysign(
-                        self.max_rotation_delta,
-                        # NB legacy use of Pygame CCW rotation here, thus negative
-                        # angle:
-                        -destination_relative_bearing,
+                        self.max_rotation_step,
+                        destination_relative_bearing,
                     ),
                 )
         self.move()
+
+    def rotate(self, rotation_delta: float) -> None:
+        """Change Bot rotation over 1 simulation step.
+
+        Parameters
+        ----------
+        rotation_delta : float
+            Rotation in degrees
+
+        """
+        # NB legacy use of Pygame CCW rotation here, thus negative angle:
+        self.heading.vector.rotate_ip(-rotation_delta)
+
+    def move(self) -> None:
+        """Change Bot position over 1 simulation step."""
+        self.pos += self.velocity * SIMULATION_STEP_INTERVAL_S
+
+    def _handle_sensing(self, other_bots: list[Bot]) -> None:
+        currently_visible_bots = {bot for bot in other_bots if self.can_see(bot)}
+        newly_spotted_bots = currently_visible_bots - self.visible_bots
+        newly_lost_bots = self.visible_bots - currently_visible_bots
+
+        if newly_spotted_bots:
+            for newly_spotted_bot in newly_spotted_bots:
+                self.notify_observers(f"I've spotted `{newly_spotted_bot.name}`")
+                self.visible_bots.add(newly_spotted_bot)
+        if newly_lost_bots:
+            for newly_lost_bot in newly_lost_bots:
+                self.notify_observers(f"I've lost sight of `{newly_lost_bot.name}`")
+                self.visible_bots.remove(newly_lost_bot)
+                self.known_bots.add(newly_lost_bot)
 
     def can_see(self, other_bot: Bot) -> bool:
         """Determine whether the Bot can see another Bot.
