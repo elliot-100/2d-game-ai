c078db47df207685e8c83c69a7e4b65e05f28229
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-09-25 15:21:00 +0100

    remove `entities._Subject' from API

--- two_d_game_ai/entities/__init__.py
+++ two_d_game_ai/entities/__init__.py
@@ -3,6 +3,5 @@
 from two_d_game_ai.entities.bot import Bot
 from two_d_game_ai.entities.generic_entity import GenericEntity
 from two_d_game_ai.entities.movement_block import MovementBlock
-from two_d_game_ai.entities.observer_pattern import _Subject
 
-__all__ = "Bot", "MovementBlock", "GenericEntity", "_Subject"
+__all__ = "Bot", "MovementBlock", "GenericEntity"
