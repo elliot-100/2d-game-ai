e392f658413921e22257cfa88eb813325e5b9de9
elliot-100 <3186037+elliot-100@users.noreply.github.com>
2025-03-15 14:36:05 +0000

    feat: reduce info-level logging

--- tests/entities/test_observer.py
+++ tests/entities/test_observer.py
@@ -16,39 +16,45 @@ class TestSubject:
 
     def test_create(self) -> None:
         """Test that a Subject is created."""
+        # arrange
+        # act
         s = Subject("S")
         assert s.name == "S"
 
     def test_notification_logged(self, caplog: pytest.LogCaptureFixture) -> None:
         """Test notification is logged by Observer."""
+        # arrange
+        s = Subject("S")
+        o = Observer("O")
+        s.register_observer(o)
         with caplog.at_level(logging.DEBUG):
-            s = Subject("S")
-            o = Observer("O")
-            s.register_observer(o)
-
+            # act
             s.notify_observers("TestDispatchMessage")
         assert "Subject 'S' notifying observers: 'TestDispatchMessage'" in caplog.text
 
     def test_notification_raises_exception_if_no_subscribers(self) -> None:
         """Test Exception is raised on notification if no Observers."""
+        # arrange
         s = Subject("S")
-
+        # act / assert
         with pytest.raises(ObserverException):
             s.notify_observers("TestDispatchMessage")
 
     def test_register_observer(self) -> None:
         """Test that an Observer is registered."""
+        # arrange
         s = Subject("S")
         o = Observer("O")
-
+        # act
         s.register_observer(o)
         assert o in s.observers
 
     def test_unregister_subscriber(self) -> None:
         """Test that a Subscriber is unregistered."""
+        # arrange
         s = Subject("S")
         o = Observer("O")
-
+        # act
         s.unregister_observer(o)
         assert o not in s.observers
 
@@ -58,15 +64,18 @@ class TestObserver:
 
     def test_create(self) -> None:
         """Test that Observer is created."""
+        # arrange
+        # act
         o = Observer("O")
         assert o.name == "O"
 
     def test_report_event(self, caplog: pytest.LogCaptureFixture) -> None:
         """Test received event is logged."""
+        # arrange
+        s = Subject("S")
+        o = Observer("O")
+        s.register_observer(o)
         with caplog.at_level(logging.DEBUG):
-            s = Subject("S")
-            o = Observer("O")
-            s.register_observer(o)
-
+            # act
             s.notify_observers("TestReportMessage")
         assert "Observer 'O' got 'TestReportMessage' from 'S'" in caplog.text
