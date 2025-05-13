a8ba9e164e73da6a4f8b6954d5c612b53aefdbb1
elliot-100 <3186037+elliot-100@users.noreply.github.com>
2025-05-13 21:48:22 +0100

    WIP

--- examples/following.py
+++ examples/following.py
@@ -4,53 +4,59 @@ import itertools
 
 from examples.logging_config import configure_logger
 from two_d_game_ai.entities.bot import Bot
-from two_d_game_ai.entities.obstacles import ObstacleCircle
+from two_d_game_ai.entities.obstacles import ObstacleRectangle
 from two_d_game_ai.view.view import View
 from two_d_game_ai.world.world import World
 
 configure_logger()
 
 # Create a World
-the_world = World(20, grid_size=32)
+the_world = World(40, grid_size=16)
 
 # Add a Bot to the World...
-leader_a = Bot(name="A", position_from_sequence=(-5, -5))
+leader_a = Bot(name="A", position_from_sequence=(-10, -10))
 the_world.add_entity(leader_a)
 
 # Group of followers:
-origin_a = -5, 5
-for ax, ay in itertools.product(range(2), range(2)):
-    position = origin_a[0] + 2 * ax, origin_a[1] + 2 * ay
+origin = -10, 10
+offset = 2, 2
+for i in range(4):
+    pos = origin[0] + offset[0] * i, origin[1] + offset[1] * i
     the_world.add_entity(
         Bot(
-            name=f"A-{ax}{ay}",
-            position_from_sequence=position,
+            name=f"A{i}",
+            position_from_sequence=pos,
             leader=leader_a,
             has_memory=True,
         )
     )
 
 # Follower chain:
-origin_b = 4, 7
-leader_b = Bot(name="B", position_from_sequence=origin_b)
+origin = 10, -10
+offset = 2, 2
+leader_b = Bot(name="B", position_from_sequence=origin)
 the_world.add_entity(leader_b)
 
 for i in range(1, 4):
-    bx = origin_b[0] + 0.5 * i
-    by = origin_b[1] - 5 * i
+    pos = origin[0] + offset[0] * i, origin[1] + offset[1] * i
     the_world.add_entity(
         Bot(
-            name=f"B-{i}",
-            position_from_sequence=(bx, by),
+            name=f"B{i}",
+            position_from_sequence=pos,
             leader=leader_b,
             has_memory=True,
         )
     )
 
-ObstacleCircle(name="mb0", position_from_sequence=(2, -1.5), radius=2)
+the_world.add_entity(
+    ObstacleRectangle(name="mb0", position_from_sequence=(-14, 0), size=(10, 2))
+)
+the_world.add_entity(
+    ObstacleRectangle(name="mb1", position_from_sequence=(2, 0), size=(10, 2))
+)
 
 # Create a View of the World
-view = View(world=the_world)
+view = View(world=the_world, show_debug_while_unpaused=True)
 
 while view.running:
     view.handle_inputs()
